03550516de44e56ade2aa36b5b5d15ee960b1cbb
Emmnanuel <147439949+Emmnanuel@users.noreply.github.com>
2023-10-20 21:56:24 +0000

    actualizo

---
+++ entrega/viernes 13.py
@@ -0,0 +1,18 @@
+print("Las máquinas me sorprenden con mucha frecuencia.")
+print(" ")
+print("DESCARGA LOL")
+print("23")
+23
+print("Una computadora puedeser llamada 'inteligente', si logra engañar a una persona haciéndole creer quees un humano.")
+print("Emmanuel", "Caceres", "18", sep="-")
+
+calle = "Calle Ejemplo"
+numero = "123"
+codigo_postal = "12345"
+
+print(calle + "\t" + numero + "\t" + codigo_postal)
+texto = "Feliz\n\tPrimavera\n\t\t2023"
+print(texto)
+
+print("Solo podemos ver poco del futuro,", end=" ")
+print("pero lo suficiente para darnos cuenta de que hay mucho que hacer.")
